7adfca85eb643f4b46c5490d8f60014f1f400716
Vebjorn0 <60983117+Vebjorn0@users.noreply.github.com>
2020-09-05 13:17:37 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 920587237 then
-    loadstring(game:HttpGet("https://github.com/Vebjorn0/ZenithHub/blob/master/test2.lua"))()
+    loadstring(game:HttpGet("https://raw.github.com/Vebjorn0/ZenithHub/blob/master/test2.lua"))()
 end
