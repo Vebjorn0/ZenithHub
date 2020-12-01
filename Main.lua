74c0dcb9533973b8b45e6cbd5401bcdcc959cfdd
Vebjorn0 <60983117+Vebjorn0@users.noreply.github.com>
2020-12-01 19:51:01 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,9 @@
-if game.PlaceId == 920587237 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Vebjorn0/ZenithHub/master/test2.lua"))()
+if game.PlaceId == 1451195595 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Vebjorn0/ZenithHub/master/FindTheNoobs2.lua"))()
+end
+if game.PlaceId == 2533391464 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Vebjorn0/ZenithHub/master/SnowmanSimulator.lua"))()
+end
+if game.PlaceId == 155615604 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Vebjorn0/ZenithHub/master/PrisonLife.lua"))()
 end
